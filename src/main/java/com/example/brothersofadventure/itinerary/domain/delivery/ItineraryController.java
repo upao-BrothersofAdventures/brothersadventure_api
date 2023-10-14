49ac2a72fa8514edfdf2854b41da5538cc94e40f
SeijiAMG <samayag2@upao.edu.pe>
2023-10-14 02:18:07 -0500

    Creacion Clase Itinerary

---
+++ src/main/java/com/example/brothersofadventure/itinerary/domain/delivery/ItineraryController.java
@@ -0,0 +1,4 @@
+package com.example.brothersofadventure.itinerary.domain.delivery;
+
+public class ItineraryController {
+}
--- src/main/java/com/example/brothersofadventure/itinerary/domain/entity/Itinerary.java
+++ src/main/java/com/example/brothersofadventure/itinerary/domain/entity/Itinerary.java
@@ -1,4 +1,67 @@
 package com.example.brothersofadventure.itinerary.domain.entity;
+import jakarta.persistence.Entity;
+        import jakarta.persistence.GeneratedValue;
+        import jakarta.persistence.GenerationType;
+        import jakarta.persistence.Id;
+    import lombok.AllArgsConstructor;
+    import lombok.Data;
+    import lombok.NoArgsConstructor;
+
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class Itinerary {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String destino;
+    private String fecha_inicio;
+
+    private String fecha_fin;
+
+    private String descripcion;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public String getFecha_inicio() {
+        return fecha_inicio;
+    }
+
+    public void setFecha_inicio(String fecha_inicio) {
+        this.fecha_inicio = fecha_inicio;
+    }
+
+    public String getFecha_fin() {
+        return fecha_fin;
+    }
+
+    public void setFecha_fin(String fecha_fin) {
+        this.fecha_fin = fecha_fin;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
